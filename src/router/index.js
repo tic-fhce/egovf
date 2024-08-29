33281b149f41c1380282fdaa3a6877aec813cfc7
Leonel Chauca <leonelchauca0@gmail.com>
2024-08-28 20:35:06 -0400

    primeros cambios - estilos - views - vueconfig

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter,createWebHistory } from 'vue-router'
 //import HomeView from '../views/HomeView.vue'
 
 import EscritorioLayout from '@/layouts/EscritorioLayout'
@@ -147,7 +147,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior(){
     return{top:0}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+const path=require('path');
 module.exports = defineConfig({
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@styles': path.resolve(__dirname, 'src/styles'),
+      },
+    },
+  },
   transpileDependencies: true
 })
